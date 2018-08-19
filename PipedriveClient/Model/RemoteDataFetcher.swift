0224912d60b654eeb611df3f49ced02bac08acbf
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-19 21:55:27 +0300

    - Add RemoteDataFetcher and UrlBuilder class prototypes.

---
+++ PipedriveClient/Model/RemoteDataFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  RemoteDataFetcher.swift
+//  PipedriveClient
+//
+//  Created by Andrius Shiaulis on 19.08.2018.
+//  Copyright © 2018 Andrius Shiaulis. All rights reserved.
+//
+
+import Foundation
---
+++ PipedriveClient/Model/UrlBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  UrlBuilder.swift
+//  PipedriveClient
+//
+//  Created by Andrius Shiaulis on 19.08.2018.
+//  Copyright © 2018 Andrius Shiaulis. All rights reserved.
+//
+
+import Foundation
