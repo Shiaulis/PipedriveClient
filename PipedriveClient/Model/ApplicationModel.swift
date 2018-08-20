063ed44d59a1d869b504998fe547b68321613c42
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-20 19:21:18 +0300

    - Implement using requestBuilder in ApplicationModel.

--- PipedriveClient/Model/ApplicationModel.swift
+++ PipedriveClient/Model/ApplicationModel.swift
@@ -12,9 +12,16 @@ import os.log
 class ApplicationModel {
 
     // MARK: - Properties -
+    static private let urlScheme = "http"
+    static private let apiVersion = "v1"
+    // To check app in your own enviroment just paste
+    // below your own companyName and token
+    static private let companyName = "andriusinc"
+    static private let token = "d95b2a784b544f23d9ccb4c3eae9b879c91225c1"
     static private let logger = OSLog.init(subsystem: LogSubsystem.applicationModel, object: ApplicationModel.self)
 
-
+    private let requestBuilder: RequestBuilder?
+    private let remoteDataFethcer: RemoteDataFetcher
     private let persistentDataManager: PersistentDataManager?
     private let persistentDataManagerError: Error?
 
@@ -31,10 +38,28 @@ class ApplicationModel {
             os_log("Failed to initiate persistent storage. Error: '%@'. Reason: '%@'.", log: ApplicationModel.logger, type: .error, error.localizedDescription, error.failureReason)
             assertionFailure()
         }
+        self.requestBuilder = RequestBuilder.init(usingURLScheme: ApplicationModel.urlScheme,
+                                                  companyName: ApplicationModel.companyName,
+                                                  apiVersion: ApplicationModel.apiVersion,
+                                                  token: ApplicationModel.token)
+        self.remoteDataFethcer = RemoteDataFetcher.init(using: DispatchQueue.global(qos: .userInteractive),
+                                                        networkProvider: URLSessionBasedNetworkProvider())
     }
 
     func setup() {
+        requestAppPersonsList()
+    }
 
+    func requestAppPersonsList() {
+        guard let url = requestBuilder?.createURL(for: .person) else {
+            os_log("Failed to create url for all persons list request", log: ApplicationModel.logger, type: .error)
+            assertionFailure()
+            return
+        }
+
+        remoteDataFethcer.performRequest(using: url) { (result) in
+
+        }
     }
 
     func saveDataInPersistentStorage() {
@@ -44,6 +69,7 @@ class ApplicationModel {
             os_log("Failed to save data to persistent storage. Error: '%@'. Reason: '%@'.", log: ApplicationModel.logger, type: .error, error.localizedDescription, error.failureReason)
             assertionFailure()
         }
-
     }
+
+    
 }
