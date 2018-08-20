446cda22405fce86f00a5c507b4d3afbe9b8e0c9
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-20 08:22:06 +0300

    - Add logging to remote data fetcher.

--- PipedriveClient/Model/RemoteDataFetcher.swift
+++ PipedriveClient/Model/RemoteDataFetcher.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import os.log
 
 enum RemoteRequestResult {
     case success(Data)
@@ -15,6 +16,7 @@ enum RemoteRequestResult {
 
 class RemoteDataFetcher {
 
+    static private let logger = OSLog.init(subsystem: LogSubsystem.applicationModel, object: RemoteDataFetcher.self)
     private let queue: DispatchQueue
     private let networkProvider: NetworkProvider
 
@@ -25,20 +27,31 @@ class RemoteDataFetcher {
 
     func performRequest(using url: URL, completionHandler: @escaping (RemoteRequestResult) -> Void) {
         queue.async { [weak self] in
+            os_log("Request for data at url '%@' started.", log: RemoteDataFetcher.logger, type: .debug, url.absoluteString)
             self?.networkProvider.dataTask(with: url, completionHandler: { (data, response, error) in
                 if let error = error {
+                    os_log("Failed to perform request for URL '%@'. Error: '%@'. Response: '%@'",
+                           log: RemoteDataFetcher.logger,
+                           type: .error,
+                           url.absoluteString, error.localizedDescription, response ?? "")
                     let result = RemoteRequestResult.failure(error)
                     completionHandler(result);
                     return
                 }
 
                 guard let receivedData = data, receivedData.count > 0 else {
+                    os_log("Failed to get data from request for URL '%@'. Response: '%@'",
+                           log: RemoteDataFetcher.logger,
+                           type: .error,
+                           url.absoluteString, response ?? "")
+
                     let result = RemoteRequestResult.failure(nil)
                     assertionFailure()
                     completionHandler(result);
                     return
                 }
 
+                os_log("Request for url '%@' succeeded.", log: RemoteDataFetcher.logger, type: .debug, url.absoluteString)
                 let result = RemoteRequestResult.success(receivedData)
                 completionHandler(result)
             })
@@ -50,7 +63,6 @@ class RemoteDataFetcher {
 /**
  Protocol encapsulates URLSession and provide an ability to test RemoteDataFetcher
  */
-
 protocol NetworkProvider {
     func dataTask(with: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)
 }
