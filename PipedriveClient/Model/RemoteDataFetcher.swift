1d3cc24ce02d70df2e9869ecd03bacfd5523fc29
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-20 08:09:47 +0300

    - Implement RemoteDataFetcher.

--- PipedriveClient/Model/RemoteDataFetcher.swift
+++ PipedriveClient/Model/RemoteDataFetcher.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+enum RemoteRequestResult {
+    case success(Data)
+    case failure(Error?)
+}
+
+class RemoteDataFetcher {
+
+    private let queue: DispatchQueue
+    private let networkProvider: NetworkProvider
+
+    init(using queue:DispatchQueue, networkProvider: NetworkProvider) {
+        self.queue = queue
+        self.networkProvider = networkProvider
+    }
+
+    func performRequest(using url: URL, completionHandler: @escaping (RemoteRequestResult) -> Void) {
+        queue.async { [weak self] in
+            self?.networkProvider.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let error = error {
+                    let result = RemoteRequestResult.failure(error)
+                    completionHandler(result);
+                    return
+                }
+
+                guard let receivedData = data, receivedData.count > 0 else {
+                    let result = RemoteRequestResult.failure(nil)
+                    assertionFailure()
+                    completionHandler(result);
+                    return
+                }
+
+                let result = RemoteRequestResult.success(receivedData)
+                completionHandler(result)
+            })
+
+        }
+    }
+}
+
+/**
+ Protocol encapsulates URLSession and provide an ability to test RemoteDataFetcher
+ */
+
+protocol NetworkProvider {
+    func dataTask(with: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)
+}
