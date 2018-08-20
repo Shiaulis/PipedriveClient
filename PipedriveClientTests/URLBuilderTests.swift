8205e50ac5c040e7632f8b5bf86b9b3378c85d0a
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-20 07:24:13 +0300

    - Update test for URLBuilder.

--- PipedriveClientTests/URLBuilderTests.swift
+++ PipedriveClientTests/URLBuilderTests.swift
@@ -10,9 +10,20 @@ import XCTest
 @testable import PipedriveClient
 
 class URLBuilderTests: XCTestCase {
-    
-    func testBuilder_initialization {
-        let buildScheme
-    }
 
+    static private let getAllPersonsURLExpectation = "https://api.pipedrive.com/v1/persons?start=0&api_token=d95b2a784b544f23d9ccb4c3eae9b879c91225c1"
+
+    func testBuilder_getAllPersons() {
+        let buildScheme = URLBuilderScheme.init(companyName: "api",
+                                                endpoint: .person,
+                                                pagination: Pagination.defaultPagination,
+                                                apiToken: ApiToken.current)
+        let urlBuilder = URLBuilder.init(using: buildScheme)
+        let url = urlBuilder.url
+
+        XCTAssertNotNil(url)
+        let stringUrl = url?.absoluteString
+        XCTAssertNotNil(stringUrl)
+        XCTAssertEqual(stringUrl, URLBuilderTests.getAllPersonsURLExpectation)
+    }
 }
