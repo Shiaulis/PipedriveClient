9b4bded59132d9fe5a9d1d17ddb17b201b22a1c1
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-21 22:34:54 +0300

    - Add using cacheStorage and file manager to Application Model.

--- PipedriveClient/Model/ApplicationModel.swift
+++ PipedriveClient/Model/ApplicationModel.swift
@@ -22,12 +22,14 @@ class ApplicationModel {
     static private let logger = OSLog.init(subsystem: LogSubsystem.applicationModel, object: ApplicationModel.self)
 
     private let requestBuilder: RequestBuilder?
-    private let remoteDataFethcer: RemoteDataFetcher
+    private let remoteDataFetcher: RemoteDataFetcher
     private let dataMapper: DataMapper
     private let persistentDataManager: PersistentDataManager?
     private let persistentDataManagerError: Error?
 
     private var personModelControllers:[PersonModelContoller]
+
+    private let cacheStorage: CacheStorage?
 
     // MARK: - Initialization -
 
@@ -46,10 +48,19 @@ class ApplicationModel {
                                                   companyName: ApplicationModel.companyName,
                                                   apiVersion: ApplicationModel.apiVersion,
                                                   token: ApplicationModel.token)
-        self.remoteDataFethcer = RemoteDataFetcher.init(using: DispatchQueue.global(qos: .userInteractive),
+        self.remoteDataFetcher = RemoteDataFetcher.init(using: DispatchQueue.global(qos: .userInteractive),
                                                         networkProvider: URLSessionBasedNetworkProvider())
         self.dataMapper = DataMapper.init(queue: .global(qos: .userInteractive))
         self.personModelControllers = []
+
+        let fileManager: LocalFileManager
+        do {
+            try fileManager = SystemLocalFileManager()
+            self.cacheStorage = try CacheStorage.init(queue: .global(qos: .userInitiated), fileManager: fileManager)
+        } catch {
+            os_log("Failed to initiate cache storage. Error: '%@'", log: ApplicationModel.logger, type: .error, error.localizedDescription)
+            self.cacheStorage = nil
+        }
     }
 
     func setup() {
@@ -57,22 +68,35 @@ class ApplicationModel {
     }
 
     func requestAppPersonsList() {
+        // 1. Create request URL
         guard let url = requestBuilder?.createURL(for: .person) else {
             os_log("Failed to create url for all persons list request", log: ApplicationModel.logger, type: .error)
             assertionFailure()
             return
         }
 
-        remoteDataFethcer.performRequest(using: url) { [weak self] (result) in
+        // 2. Perform request by network
+        remoteDataFetcher.performRequest(using: url) { [weak self] (result) in
             switch result {
             case .success(let data):
                 guard let strongSelf = self else {
                     assertionFailure()
                     return
                 }
+
+                // 3. Decode information from received data
                 strongSelf.decodePersons(from: data, completionHandler: { (personModelControllers) in
                     if let personModelControllers = personModelControllers {
+                        // 4. Save parsed information to runtime storage
                         strongSelf.personModelControllers = personModelControllers
+                        // 5. Cache data to file system
+                        try? strongSelf.cacheStorage?.cache(data: data, forCategory: .allPersons, completionHandler: { (error) in
+                            if let error = error {
+                                os_log("Failed to cache received data. Error: '%@'", log: ApplicationModel.logger, type: .error, error.localizedDescription)
+                                return
+                            }
+                            os_log("Data successfully cached.", log: ApplicationModel.logger, type: .debug)
+                        })
                     }
                     else {
                         os_log("Failed to get person model controllers for remote request.", log: ApplicationModel.logger, type: .error)
