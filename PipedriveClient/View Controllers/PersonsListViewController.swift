94f1c5fef845d3289f2b7f2844f0f0bf6c2a7c2e
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-22 21:46:57 +0300

    - Add implementation for person view model and person view controller.

--- PipedriveClient/View Controllers/PersonsListViewController.swift
+++ PipedriveClient/View Controllers/PersonsListViewController.swift
@@ -13,6 +13,7 @@ class PersonsListViewController: UITableViewController {
     // MARK: - Properties -
 
     static private let controllerTitle = NSLocalizedString("Contacts", comment: "controller title")
+    static private let cellID = "PersonsListViewControllerCellID"
 
     private let viewModel: PersonsListViewModel
 
@@ -28,6 +29,7 @@ class PersonsListViewController: UITableViewController {
     // MARK: - View controller life cycle -
 
     override func viewDidLoad() {
+        setupViewModel()
         setupView()
         setupNavigationBar()
         setupTableView()
@@ -35,9 +37,61 @@ class PersonsListViewController: UITableViewController {
 
     // MARK: - Table view data source methods
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfCells
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PersonsListViewController.cellID, for: indexPath) as? PersonTableViewCell else {
+            assertionFailure()
+            return UITableViewCell()
+        }
+
+//        guar let modelController = viewModel.modelController(for: indexPath) else {
+//            assertionFailure()
+//            return UITableViewCell()
+//        }
+
+
+//        cell.nameLabel.text = cellVM.titleText
+//        cell.descriptionLabel.text = cellVM.descText
+//        cell.mainImageView?.sd_setImage(with: URL( string: cellVM.imageUrl ), completed: nil)
+//        cell.dateLabel.text = cellVM.dateText
+
+        return cell
+
+    }
 
 
     // MARK: - Private methods -
+
+    private func setupViewModel() {
+        viewModel.showAlertBlock = { [weak self] () in
+            DispatchQueue.main.async {
+                if let message = self?.viewModel.alertMessage {
+                    self?.showAlert( message )
+                }
+            }
+        }
+
+        viewModel.updateLoadingStatus = { [weak self] () in
+            DispatchQueue.main.async {
+                let isLoading = self?.viewModel.isLoading ?? false
+                if isLoading {
+                    self?.tableView.refreshControl?.beginRefreshing()
+                }
+                else {
+                    self?.tableView.refreshControl?.endRefreshing()
+                }
+            }
+        }
+
+        viewModel.reloadTableViewBlock = { [weak self] () in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }
 
     // MARK: Setup views
 
@@ -50,14 +104,25 @@ class PersonsListViewController: UITableViewController {
     }
 
     private func setupTableView() {
+        tableView.register(PersonTableViewCell.self, forCellReuseIdentifier: PersonsListViewController.cellID)
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControllPulledAction), for: .valueChanged)
         tableView.refreshControl = refreshControl
     }
 
-    // MARK: - Targeted actions -
+    // MARK: Targeted actions
 
     @objc func refreshControllPulledAction() {
+        viewModel.refreshModel()
     }
+
+    // MARK: -
+
+    private func showAlert( _ message: String ) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction( UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
 
--- PipedriveClient/View Models/PersonsListViewModel.swift
+++ PipedriveClient/View Models/PersonsListViewModel.swift
@@ -11,12 +11,71 @@ import Foundation
 class PersonsListViewModel {
 
     // MARK: - Properties -
+    var showAlertBlock: (() -> Void)?
+    var updateLoadingStatus: (() -> Void)?
+    var reloadTableViewBlock: (() -> Void)?
+    var isLoading: Bool = false {
+        didSet {
+            self.updateLoadingStatus?()
+        }
+    }
+    var alertMessage: String? {
+        didSet {
+            self.showAlertBlock?()
+        }
+    }
+    var numberOfCells: Int {
+        return personModelControllers.count
+    }
+
 
     private let dataProvider: DataProvider
+    private var personModelControllers: [PersonModelContoller] {
+        didSet {
+            self.reloadTableViewBlock?()
+        }
+    }
 
     // MARK: - Initialization -
 
     init(using dataProvider: DataProvider) {
         self.dataProvider = dataProvider
+        self.personModelControllers = []
+    }
+
+    // MARK: - Public methods -
+
+    func refreshModel() {
+        dataProvider.updatePersonsModelsFromRemoteServer { (refreshModelResult) in
+            self.isLoading = false
+            switch refreshModelResult {
+            case .failure(let error):
+                self.alertMessage = error.localizedDescription
+            case .success (let personModelControllers):
+                self.personModelControllers = personModelControllers
+            }
+        }
+    }
+
+    func personCellViewModel(for indexPath: IndexPath ) -> PersonCellViewModel? {
+        guard indexPath.row < personModelControllers.count else {
+            assertionFailure()
+            return nil
+        }
+
+        return PersonCellViewModel(using: personModelControllers[indexPath.row])
+    }
+}
+
+struct PersonCellViewModel {
+
+    // MARK: - Properties -
+
+    private let modelController: PersonModelContoller
+
+    // MARK: - Initialization -
+
+    init(using modelController: PersonModelContoller) {
+        self.modelController = modelController
     }
 }
