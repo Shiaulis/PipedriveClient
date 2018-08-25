c9fa84693c46f13b42c22979a7d12d6a1abff613
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-25 17:35:41 +0300

    - Add login and sign in classes prototypes.

---
+++ PipedriveClient/View Controllers/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  PipedriveClient
+//
+//  Created by Andrius Shiaulis on 25.08.2018.
+//  Copyright © 2018 Andrius Shiaulis. All rights reserved.
+//
+
+import Foundation
