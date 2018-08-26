85c28d40ae4610d77360bc4c3b4832cea947c27e
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-26 20:18:33 +0300

    - Add placeholder view.

---
+++ PipedriveClient/Views/PlaceholderView.swift
@@ -0,0 +1,39 @@
+//
+//  PlaceholderView.swift
+//  PipedriveClient
+//
+//  Created by Andrius Shiaulis on 26.08.2018.
+//  Copyright © 2018 Andrius Shiaulis. All rights reserved.
+//
+
+import UIKit
+
+class PlaceholderView: UILabel {
+
+    // MARK: - Initialization -
+
+    convenience init() {
+        self.init(usingTitle: NSLocalizedString("The list is empty", comment: "placeholder text"))
+    }
+
+    init(usingTitle title: String) {
+        super.init(frame: .zero)
+        self.text = title
+        setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private methods -
+
+    private func setupView() {
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title1)
+        lineBreakMode = .byWordWrapping
+        numberOfLines = 0
+        textColor = UIColor.lightGray
+    }
+}
+
